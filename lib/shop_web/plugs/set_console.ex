175c7a36d652ff9f04da7f8eb17a7137e4385494
“GrizzlyTaro” <gowthamflcn007@gmail.com>
2025-05-19 22:58:25 +0530

    updated plugs

--- lib/shop_web/plugs/set_console.ex
+++ lib/shop_web/plugs/set_console.ex
@@ -1,9 +1,26 @@
 defmodule ShopWeb.Plugs.SetConsole do
+  import Plug.Conn
+
+  @valid_consoles ["pc", "playstation", "nintendo", "xbox"]
+
   def init(opts), do: opts
 
-  def call(%Plug.Conn{} = conn, opts) do
-    IO.inspect("Module Plug is working")
-    IO.inspect("Opts: " <> opts)
+  def call(%Plug.Conn{:params => %{"console" => console}} = conn, _opts)
+      when console in @valid_consoles do
     conn
+    |> assign(:console, console)
+    |> put_resp_cookie("console", console, max_age: :timer.hours(24) * 30)
+  end
+
+  def call(%Plug.Conn{:cookies => %{"console" => console}} = conn, _opts)
+      when console in @valid_consoles do
+    conn
+    |> assign(:console, console)
+  end
+
+  def call(%Plug.Conn{} = conn, default_console) do
+    conn
+    |> assign(:console, default_console)
+    |> put_resp_cookie("console", default_console, max_age: :timer.hours(24) * 30)
   end
 end
